0f23750ee1ec637d3227521188b10e4a65572de2
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-08-24 15:56:28 +0900

    Update adaptor.py

--- design-patterns/structural-patterns/adaptor.py
+++ design-patterns/structural-patterns/adaptor.py
@@ -1,1 +1,20 @@
+class Target:
+    def request(self):
+        pass
 
+class Adaptee:
+    def specific_request(self):
+        return "Specific request"
+
+class Adapter(Target):
+    def __init__(self, adaptee):
+        self.adaptee = adaptee
+
+    def request(self):
+        return self.adaptee.specific_request()
+
+def client_code(target):
+    print(target.request())
+
+adapter = Adapter(Adaptee())
+client_code(adapter)
