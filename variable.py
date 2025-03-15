23ffd2fc45408d64f59c09970d15dacd3b7d4492
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2025-03-15 13:29:09 +0900

    Update variable.py

--- variable.py
+++ variable.py
@@ -1,1 +1,22 @@
+class Variable:
+    cls_variable = "CLASS"
 
+    def change_on_instance(self):
+        self.cls_variable = "Instance"
+
+    @classmethod
+    def change_on_class(cls):
+        cls.cls_variable = "Class"
+
+
+print(Variable.cls_variable) # "CLASS"
+print(Variable().cls_variable) # "CLASS"
+
+Variable.change_on_class()
+print(Variable.cls_variable) # "Class"
+print(Variable().cls_variable) # "Class"
+
+variable = Variable()
+variable.change_on_instance()
+print(variable.cls_variable) # "Instance"
+print(Variable.cls_variable) # "Class"
